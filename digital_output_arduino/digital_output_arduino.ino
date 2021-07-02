c0d87a774ea304c4d35af442d6b1741f15f4bbdd
anagamori <nagamori@usc.edu>
2021-07-02 14:43:26 -0700

    Create digital_output_arduino.ino

---
+++ digital_output_arduino/digital_output_arduino.ino
@@ -0,0 +1,15 @@
+unsigned long halfPeriod = 1000UL; // 5mS = 1/2 100 Hz 
+// don't use a floating # for delay, delayMicroseconds
+void setup()
+{
+pinMode(13, OUTPUT);  
+digitalWrite (13, HIGH); // known starting level
+}
+
+void loop(){
+while (1){ // avoid  loop() jitter
+  PINB = PINB | 0b00101000; // x-x-13-12-11-10-9-8 on Uno, 
+                                             // toggle output by writing 1 to input register
+  delayMicroseconds(halfPeriod);
+  } 
+}
