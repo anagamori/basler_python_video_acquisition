f14541345ffce46f71be21be8ff9dccb092d9998
Akira Nagamori <nagamori@usc.edu>
2023-09-15 17:25:02 -0700

    update

---
+++ crop_video.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 09:12:17 2023
+
+@author: anaga
+"""
+
+import os
+from os import listdir
+from os.path import isfile, join
+from imageio import get_writer
+import _pickle as cPickle
+
+ext = '.mp4'
+fps = 200
+fps_new = 20
+
+start_frame = int(2.2*fps)
+end_frame = int(3.0*fps)
+frame_str = '_' + str(start_frame) + '_' + str(end_frame)
+idx = range(start_frame,end_frame)
+
+filename = "C:\\Users\\anaga\\OneDrive\\Desktop\\Research\\Presentation\\U19\\All-hands\\Videos\\CFL18_04112023_saline_1_15-39-03_cam2"
+infile = open(filename, "rb" )
+data = cPickle.load(infile)
+# cannot use quality = 10 if you want to play it on powerpoint 
+writer = get_writer(filename + frame_str +ext,fps = fps_new)
+for j in range(len(idx)):
+    idx_frame = idx[j]
+    img_temp = data[idx_frame] 
+    writer.append_data(img_temp) 
+    
+
+infile.close()        
+writer.close()
